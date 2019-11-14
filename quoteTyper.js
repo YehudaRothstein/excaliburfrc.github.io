6e2fb1689616b805f7e8aa049c3df96c90426db8
renderhaf <dvir.akerman@gmail.com>
2019-11-14 08:12:56 +0200

    New Website Design

---
+++ quoteTyper.js
@@ -0,0 +1,69 @@
+//place quotes
+let quotes = document.getElementById("qtrt");
+let quoteArr = '["Aim for the stars. The worst that can happen is that you&#39;ll get to the moon.",'
+quoteArr += '"Writing is the exact opposite of Forgetting.",'
+quoteArr += '"If I ever ask you what is supposed to happen, the answer is never a meeting.",'
+quoteArr += '"If we run out of money, we&#39;ll get to the campionship via public transportation"'
+quoteArr += ']'
+quotes.setAttribute("data-rotate", quoteArr);
+
+//quote rotate
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    setTimeout(
+        this.tick(),
+        this.period
+    )
+    this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 150 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+var elements = document.getElementsByClassName('txt-rotate');
+for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+    new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+}
+// INJECT CSS
+var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+};

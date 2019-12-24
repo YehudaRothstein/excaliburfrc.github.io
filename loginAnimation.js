213dbcdcff99bdb8360aaaffe4c35fd96f7b389f
renderhaf <dvir.akerman@gmail.com>
2019-12-24 12:43:40 +0200

    added loading for login

---
+++ loginAnimation.js
@@ -0,0 +1,5 @@
+function goToLogin(){
+    document.getElementsByClassName("load-anim-1")[0].style.display = "inline-block";
+    document.getElementById("loginicon").style.display = "none";
+    window.location = "https://excalibur-private-server.herokuapp.com/";
+}

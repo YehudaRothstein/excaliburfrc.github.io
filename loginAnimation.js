9f0cb1eb23de73a16b3f6c9fb14b1ef8e8d7df0d
Starlight220 <yotamshlomi@gmail.com>
2020-11-01 20:19:24 +0200

    remove login

--- loginAnimation.js
+++
@@ -1,5 +0,0 @@
-function goToLogin(){
-    document.getElementsByClassName("load-anim-1")[0].style.display = "inline-block";
-    document.getElementById("loginicon").style.display = "none";
-    window.location = "https://excalibur-private-server.herokuapp.com/";
-}
